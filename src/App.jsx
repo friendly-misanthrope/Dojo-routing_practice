fe228b884e8a7e325d80d9e5e35e2601ff1174f1
NickBarkerDev <nickthedevguy@gmail.com>
2023-04-04 23:34:15 -0700

    added all 4 routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Number from './components/Number'
 import Word from './components/Word'
@@ -6,7 +7,12 @@ import Word from './components/Word'
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/:number' element={<Number />} />
+        <Route path='/:word' element={<Word />} />
+        <Route path='/:word/:textColor/:bgColor' element={<Word />} />
+      </Routes>
       
     </div>
   );
