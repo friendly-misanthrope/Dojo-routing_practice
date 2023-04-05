1680b54581932e78ee88f57d304bf8b1e634a72d
NickBarkerDev <nickthedevguy@gmail.com>
2023-04-04 23:26:23 -0700

    added Home, Number, and Word components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css';
+import Home from './components/Home'
+import Number from './components/Number'
+import Word from './components/Word'
 
 function App() {
   return (
     <div className="App">
+      
       
     </div>
   );
