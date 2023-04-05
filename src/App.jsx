18d676f90f9fa54c406b81f120817d66497b2b70
NickBarkerDev <nickthedevguy@gmail.com>
2023-04-05 00:34:00 -0700

    Added isNaN check to conditionally render path variables based on data type

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<GoHome />} />
         <Route path='/home' element={<Home />} />
-        <Route path='/:word' element={<DisplayParameters />} />
+        <Route path='/:hello' element={<DisplayParameters />} />
         {/* <Route /> */}
         
       </Routes>
--- src/components/DisplayParameters.jsx
+++ src/components/DisplayParameters.jsx
@@ -1,10 +1,21 @@
 import { useParams } from 'react-router-dom';
 
 const DisplayParameters = (props) => {
+
+  const { hello, txtColor, bgColor } = useParams()
+
   return (
     <div>
       {
-        
+        isNaN(hello) ?
+        <h3 style={
+          txtColor ? 
+          {color: txtColor, bgColor: bgColor}
+          : null
+        }>
+
+          The word is: {hello}</h3>
+        : <h3>The number is: {hello}</h3>
       }
     </div>
   );
